ee3f8be5a198bc4b82ca32e2d970a3b0e28b93c3
sosinatolossa <sosinatolossa16@gmail.com>
2020-12-08 14:17:13 -0600

    added comments

--- scripts/main.js
+++ scripts/main.js
@@ -3,3 +3,20 @@ import {ConvictionSelect} from "./convictions/ConvictionSelect.js"
 
 CriminalList()
 ConvictionSelect()
+
+// TODO: to filter out criminals based on the crime they've commited
+// What feature are we implementing?
+// Filter our criminals by the crimes committed
+
+
+// What tasks do we need to complete to implement the feature?
+// filter the criminal data by matching the crime that has been selected
+
+// listen for the selection of a crime and capture the chosen value
+// Use the selected value to filter the criminal data
+// Update the DOM with the filtered criminal data
+
+
+// Which modules are involved?
+// CriminalList
+// ConvictionSelect
